d370099efab0c914b634e7a1941478c60b6b73fd
FloRul <rumiel.florian@gmail.com>
2024-05-18 12:51:34 -0400

    first commit

---
+++ src/robco_runner.py
@@ -0,0 +1,59 @@
+﻿from pathlib import Path
+import time
+from typing import Optional, Tuple, Union
+from fmeval.model_runners.model_runner import ModelRunner
+import json
+import websocket
+
+
+class RobcoRunner(ModelRunner):
+    def __init__(self, ws_address: str, *args, **kwargs):
+        self.ws_address = ws_address
+        self.ws = None
+        self.connect()
+
+    def connect(self):
+        conn = websocket.create_connection(self.ws_address, timeout=60)
+        self.ws = conn
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state["ws"]
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.connect()
+
+    def predict(self, prompt: str) -> Tuple[Optional[str], Optional[float]]:
+        request_params = {
+            "message": prompt,
+        }
+        request_json = json.dumps(request_params)
+
+        try:
+            # Send the request
+            self.ws.send(request_json)
+            # Receive the response
+            response_json = self.ws.recv()
+            # Convert the JSON string to a dictionary
+            response_dict = json.loads(response_json)
+            # Extract the result from the response dictionary
+            result = response_dict.get("message")
+            return result, None
+        except websocket.WebSocketTimeoutException:
+            print("WebSocket timeout, retrying...")
+            time.sleep(5)  # Wait before retrying
+            self.connect()  # Reconnect the WebSocket
+            try:
+                self.ws.send(request_json)
+                response_json = self.ws.recv()
+                response_dict = json.loads(response_json)
+                result = response_dict.get("message")
+                return result, None
+            except Exception as e:
+                print(f"Failed after retry: {e}")
+                return None, None
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None, None
---
+++ src/utils/utils.py
@@ -0,0 +1,31 @@
+﻿import binascii
+import csv
+import json
+import os
+
+
+def csv_to_jsonl(csv_file_path: str, jsonl_file_path: str):
+    with open(csv_file_path, "r", encoding="utf8") as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=";")
+        data = list(reader)
+
+    with open(jsonl_file_path, "w", encoding="utf8") as jsonl_file:
+        for row in data:
+            json.dump(row, jsonl_file)
+            jsonl_file.write("\n")
+
+
+def jsonl_to_csv(jsonl_file_path: str, csv_file_path: str):
+    with open(jsonl_file_path, "r", encoding="utf8") as jsonl_file:
+        data = [json.loads(line) for line in jsonl_file]
+
+    with open(csv_file_path, "w", encoding="utf8") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=data[0].keys(), delimiter=";")
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def get_salt():
+    # Generate a salt of 16 bytes
+    salt = os.urandom(4)
+    return binascii.hexlify(salt).decode()
